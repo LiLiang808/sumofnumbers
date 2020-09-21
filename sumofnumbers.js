faec5683ce0019c66458203b52d02da9f6228898
Li Liang <70745380+LiLiang808@users.noreply.github.com>
2020-09-21 07:02:30 -1000

    Finished WOD

---
+++ sumofnumbers.js
@@ -0,0 +1,36 @@
+const test = [1, 2,3,4,5];
+
+function sumFor(nums) {
+  let total =0;
+  for(const num of nums){
+    total+=num;
+  }
+  return total;
+}
+console.log(sumFor(test));
+
+function sumWhile(nums){
+  let total = 0;
+  let x = 0;
+  while(x<nums.length){
+    total+=nums[x];
+    x++;
+  }
+  return total;
+}
+console.log(sumWhile(test));
+
+function sumRecursion(nums){
+  if(nums.length ===0){
+    return 0;
+  }
+    return nums[0] + sumRecursion(nums.slice(1,nums.length));
+}
+
+console.log(sumRecursion(test));
+
+function sumTheSimpleWay(nums){
+  return _.reduce(nums, function(memo, num){return memo+num;},0);
+}
+
+console.log(sumTheSimpleWay(test));
